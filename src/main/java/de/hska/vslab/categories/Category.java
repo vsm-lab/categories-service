be34dea89553d00aed78cc395c748424cdb4420b
Marcel Huber <git@marcelhuber.de>
2022-04-14 12:43:41 +0200

    First API draft

---
+++ src/main/java/de/hska/vslab/categories/Category.java
@@ -0,0 +1,31 @@
+package de.hska.vslab.categories;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
